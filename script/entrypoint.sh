e28cbd9a8824f6c8fe1d616ff2cf4ffaceb3999d
Eddy0644 <30020507+Eddy0644@users.noreply.github.com>
2024-12-16 00:56:43 +0800

    Update entrypoint.sh

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 printf "nameserver 127.0.0.11\nnameserver 8.8.4.4\nnameserver 223.5.5.5\n" > /etc/resolv.conf
+
+CONFIG_FILE="/dashboard/data/config.yaml"
+# Check if the file does not exist
+if [ ! -f "$CONFIG_FILE" ]; then
+    # Ensure the directory exists
+    mkdir -p "$(dirname "$CONFIG_FILE")"
+    # Create the file with the specified content
+    cat << EOF > "$CONFIG_FILE"
+debug: false
+listenport: 8008
+language: zh_CN
+sitename: "Eddy Node Status"
+installhost: 8100
+tls: true
+EOF
+    echo "Config file created at $CONFIG_FILE"
+else
+    echo "Config file already exists at $CONFIG_FILE"
+fi
+
 exec /dashboard/app
